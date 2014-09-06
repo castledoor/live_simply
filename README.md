47a093a143acf486851044b349f0b8581750e06c
Brittany Winston <brittany@finedesigngroup.com>
2014-09-06 11:28:52 -0700

    initial commit

--- README.md
+++
@@ -1,9 +0,0 @@
-live_simply
-===========
-
-LIVE SIMPLY is a project to explore SVG animations 
-
-<p> Live Simply is made up of excerpts from a book series I find incredibly fascinating. The series contains first hand accounts of people's experiences living simply and the tips and tricks they have learned through doing so. The ones who paved the way for the <a class="link" href="http://en.wikipedia.org/wiki/Small_house_movement">tiny house movement.</a> I take no credit for the words that appear on this site, they were curated and published by Bert and Holly Davis</p>
-
-<p> If you are interested in checking out the series you can find them<a class="link" href="http://www.amazon.com/s/?ie=UTF8&keywords=dwelling+portably&tag=googhydr-20&index=aps&hvadid=44463311461&hvpos=1t1&hvexid=&hvnetw=g&hvrand=4021489492459533447&hvpone=&hvptwo=&hvqmt=e&hvdev=c&ref=pd_sl_15z1i1poic_e"> Here </a></p>
-<p>But more importantly I needed a side project to practice animating things, hopefully the quality will improve over time</p>
--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,71 @@
-   $(function() {
-        $('#pagination').pagination({
-            pages:2,
-            cssStyle: 'custom-theme',
-            onPageClick: function(pageNumber){test(pageNumber)},
-            onInit: function(){pageOne()},
-            nextText: '<i class="glyphicon glyphicon-chevron-right"></i>',
-            prevText: '<i class="glyphicon glyphicon-chevron-left"></i>'
-           
-        });
-    });
+// $(function() {
+//    $(".enter").click(function() {
+//     $('#enter').fadeOut();
+//     $('.background').fadeOut(2000);
+//   });
+// });
 
-function test(pageNumber)
+var TimeToFade = 1000.0;
+
+function animateFade(lastTick, eid)
+{  
+  var curTick = new Date().getTime();
+  var elapsedTicks = curTick - lastTick;
+  
+  var element = document.getElementById(eid);
+ 
+  if(element.FadeTimeLeft <= elapsedTicks)
+  {
+    element.style.opacity = element.FadeState == 1 ? '1' : '0';
+    element.style.filter = 'alpha(opacity = ' + (element.FadeState == 1 ? '100' : '0') + ')';
+    element.FadeState = element.FadeState == 1 ? 2 : -2;
+    return;
+  }
+ 
+  element.FadeTimeLeft -= elapsedTicks;
+  var newOpVal = element.FadeTimeLeft/TimeToFade;
+  if(element.FadeState == 1)
+    newOpVal = 1 - newOpVal;
+
+  element.style.opacity = newOpVal;
+  element.style.filter = 'alpha(opacity = ' + (newOpVal*100) + ')';
+  
+    setTimeout(function(){animateFade(curTick,eid)}, 33);
+}
+
+
+function fade(eid)
 {
-
-  var page="#page-"+pageNumber;
-  $('.selection').hide()
-  $(page).show()
-
+  var element = document.getElementById(eid);
+  
+   
+  if(element.FadeState == null)
+  {
+    if(element.style.opacity == null || element.style.opacity == '' 
+       || element.style.opacity == '1')
+      element.FadeState = 2;
+    else
+      element.FadeState = -2;
+  }
+    
+  if(element.FadeState == 1 || element.FadeState == -1)
+  {
+    element.FadeState = element.FadeState == 1 ? -1 : 1;
+    element.FadeTimeLeft = TimeToFade - element.FadeTimeLeft;
+  }
+  else
+  {
+    element.FadeState = element.FadeState == 2 ? -1 : 1;
+    element.FadeTimeLeft = TimeToFade;
+    setTimeout(animateFade(new Date().getTime(),eid), 33);
+  }  
 }
 
-function pageOne() {
-	var page_1 = "#page-1";
-	$('.selection').hide()
-	$(page_1).show()
-}
 
+$(document).ready(function(){
+    $("#fadeButton").click(function(){
+        fade('fadeBlock');
+        setInterval(function () {location.href = "main.html"}, 800)
+    });
+});
 
